ca32b71bfbb4f346a7074f772fab90887658f08d
Gerald Ruiz <gruizcavero@uh.edu>
2022-04-26 22:45:04 -0500

    added router package and changed layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,66 +3,32 @@ import "./App.css";
 import { CdsDivider } from "@cds/react/divider";
 import Header from "./components/Header/Header";
 import SideNav from "./components/SideNav/SideNav";
-import SideForm from "./components/SideForm/SideForm";
 import JsonViewer from "./customComponents/JsonViewer";
-
 
 function App() {
   return (
-    <div
-      cds-layout="vertical align:horizontal-stretch"
-      style={{ height: "100vh" }}
-    >
+    <div cds-layout="vertical align:stretch">
       <Header />
 
-      <div cds-layout="horizontal align:vertical-stretch">
+      <div cds-layout="horizontal align:vertical-stretch wrap:none">
         <SideNav />
 
         <CdsDivider orientation="vertical"></CdsDivider>
 
-        <div cds-layout="p:md p@md:lg align:stretch">
-          <JsonViewer />
+        <div cds-layout="vertical align:stretch">
+          <div className="demo-content demo-scrollable-content">
+            <div cds-layout="vertical gap:md p:md">
+              <JsonViewer />
+            </div>
+          </div>
+
+          <CdsDivider></CdsDivider>
+
+          <footer cds-layout="p-y:md p-x:lg">footer</footer>
         </div>
-
-        <CdsDivider orientation="vertical"></CdsDivider>
-
-        <section cds-layout="p:md p@md:xl">
-          <SideForm />
-        </section>
       </div>
-
-      {/* <CdsDivider></CdsDivider>
-
-      <footer cds-layout="p:md p@md:lg">footer</footer> */}
     </div>
   );
 }
 
 export default App;
-
-// <div>
-//   <header className="header-6">
-//     <div className="branding">
-//       <a href="..." className="nav-link">
-//         {/* <img className="clr-icon" src={logo} /> */}
-//         <span className="title">cicloud</span>
-//       </a>
-//     </div>
-//     <div className="header-nav">
-//       <a href="/dashboard" className="active nav-link">
-//         <span className="nav-text">Dashboard</span>
-//       </a>
-//       <a href="/projects" className="nav-link">
-//         <span className="nav-text">Projects</span>
-//       </a>
-//       <a href="/infrastructure" className="nav-link">
-//         <span className="nav-text">Infrastructure</span>
-//       </a>
-//     </div>
-//     <div className="header-actions">
-//       <a href="..." className="nav-link nav-icon">
-//         {/* <clr-icon shape="cog" /> */}
-//       </a>
-//     </div>
-//   </header>
-// </div>
--- src/customComponents/JsonViewer.tsx
+++ src/customComponents/JsonViewer.tsx
@@ -1,57 +1,56 @@
 import React, { Component } from "react";
 import ReactJson from "react-json-view";
 
-
-
 function getExampleJson2() {
   return {
-      normalized: {
-          "1-grams": {
-              body: 1,
-              testing: 1
-          },
-          "2-grams": {
-              "testing body": 1
-          },
-          "3-grams": {}
+    normalized: {
+      "1-grams": {
+        body: 1,
+        testing: 1,
       },
-      noun_phrases: {
-          body: 1
+      "2-grams": {
+        "testing body": 1,
       },
-      lemmatized: {
-          "1-grams": {
-              test: 1,
-              body: 1
-          },
-          "2-grams": {
-              "test body": 1
-          },
-          "3-grams": {}
+      "3-grams": {},
+    },
+    noun_phrases: {
+      body: 1,
+    },
+    lemmatized: {
+      "1-grams": {
+        test: 1,
+        body: 1,
       },
-      dependency: {
-          "1-grams": {
-              testingVERBROOTtestingVERB: 1,
-              bodyNOUNdobjtestingVERB: 1
-          },
-          "2-grams": {
-              "testingVERBROOTtestingVERB bodyNOUNdobjtestingVERB": 1
-          },
-          "3-grams": {}
-      }
-  }
+      "2-grams": {
+        "test body": 1,
+      },
+      "3-grams": {},
+    },
+    dependency: {
+      "1-grams": {
+        testingVERBROOTtestingVERB: 1,
+        bodyNOUNdobjtestingVERB: 1,
+      },
+      "2-grams": {
+        "testingVERBROOTtestingVERB bodyNOUNdobjtestingVERB": 1,
+      },
+      "3-grams": {},
+    },
+  };
 }
 
 export default class JsonViewer extends Component {
   render() {
     return (
-      <ReactJson
-        theme="pop"
-        collapsed={false}
-        name="large_array"
-        groupArraysAfterLength={50}
-        displayDataTypes={false}
-        src={getExampleJson2()}
-      />
+      <div style={{ width: "100%" }}>
+        <ReactJson
+          theme="pop"
+          collapsed={false}
+          name="large_array"
+          displayDataTypes={false}
+          src={getExampleJson2()}
+        />
+      </div>
     );
   }
 }
